c3f800e00000272f4922204cf5b651dc7b92e067
Rajini Mohanram <m.rajini.msc@gmail.com>
2023-08-15 18:11:07 +0200

    Liveness API added

--- src/main/java/com/wipro/springmvcjenkindemo/controller/ProductController.java
+++ src/main/java/com/wipro/springmvcjenkindemo/controller/ProductController.java
@@ -1,8 +1,12 @@
 package com.wipro.springmvcjenkindemo.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ProductController {
-
+	@GetMapping("/liveness")
+	public String getApplicationLiveness() {
+		return "Project is running...";
+	}
 }
